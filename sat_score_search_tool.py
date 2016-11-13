a045e5b479648c9715b4a7f4cdd161c9131375cb
scott.underwood87 <scott.underwood87@gmail.com>
2016-11-13 11:57:20 -0500

    cleaned up formatting in the document

--- sat_score_search_tool.py
+++ sat_score_search_tool.py
@@ -1,34 +1,30 @@
 import numpy as np
 import pandas as pd
 import datetime
-#urllib install has not been sucessfully completed in this container
 import urllib
 
-# Read in our data. We've aggregated it by date already, so we don't need to worry about paging
 
 # source data: https://data.cityofnewyork.us/Education/SAT-Results/f9bf-2cp4
-
 query = ("https://data.cityofnewyork.us/resource/734v-jeq5.json?")
 raw_data = pd.read_json(query)
 
-# Augment the data frame with the day of the week and the start of the week that it's in.
-# This will make more sense soon...   
 
 # removes all of the rows where no test takes produced a value of 's' in the score columns which prevented my from changing the column type to int
 raw_data_no_s = raw_data[raw_data.num_of_sat_test_takers != 's']  
+
 
 # these change the datatype in all of the score columns to int so that I can run calculations off of them  
 raw_data_no_s['sat_math_avg_score'] = raw_data_no_s['sat_math_avg_score'].astype(int)
 raw_data_no_s['sat_critical_reading_avg_score'] = raw_data_no_s['sat_critical_reading_avg_score'].astype(int)
 raw_data_no_s['sat_writing_avg_score'] = raw_data_no_s['sat_writing_avg_score'].astype(int)
 
-# creates a new column in the dataframe and then populate it with the average of the three sat scores
 
+# creates a new column in the dataframe and then populate it with the average of the three sat scores
 raw_data_no_s['overall_average_score'] = (raw_data_no_s.sat_writing_avg_score + raw_data_no_s.sat_critical_reading_avg_score + raw_data_no_s.sat_math_avg_score) / 3
 
-# user input prompted to indicate which score they want to sort by, if they want highest or lowest scoring schools, and how many to show
 
-#added in print lines in attmpt to make the input prompts easier to see
+# user input prompted to indicate which score they want to sort by, if they want highest or lowest scoring schools, and how many to show
+# added in print lines in attmpt to make the input prompts easier to see
 print ""
 search_target = raw_input("What specific sat score are you looking to rank schools by? (math, writing, critical reading, overall) ")
 print ""
@@ -36,7 +32,11 @@ search_type = raw_input("Are you looking for the best schools or the worst? ")
 print ""
 search_size = raw_input("How long a list do you need? ")
 
-#added in s_target as input for function
+# NEED - create a function that tests search_size input to make sure its just a number
+
+# NEED - need to figure out how to fix the error message that python is returning when i run the program
+
+# a function that prints list of schools based on user input search parameters
 def search_function(s_type, s_target):
   
   stop_trigger_1 = True
@@ -45,10 +45,9 @@ def search_function(s_type, s_target):
   s_type_lower = s_type.lower()
   s_target_lower = s_target.lower()
   
-  # creating an if statement that will translate s_target into the correct sorting column based on user input
-  
-  # fix the invalid response so that it communicates what needs to be changed
-  
+  # NEED - fix the invalid response so that it communicates what needs to be changed
+
+  # an if statement that will translate s_target into the correct sorting column based on user input
   while stop_trigger_1 == True:
     if s_target_lower == "math":
       s_target_input = "sat_math_avg_score"
@@ -65,9 +64,9 @@ def search_function(s_type, s_target):
     else:
       print "Invalid Option"
       s_target = raw_input("What specific sat score are you looking to rank schools by? (math, writing, critical reading, overall)")
-      
-  # changed the sort value from "overall_average_score" to the value of the s_target_input variable
   
+  # an if statement that sorts scores high-low or low-high based on user input, also ensures that sort is also run on the right column of scores
+  # changed the sort value from "overall_average_score" to the value of the s_target_input variable  
   while stop_trigger_2 == True:
     if s_type_lower == "best":
       sorted_raw_data_no_s = raw_data_no_s.sort_values(s_target_input, ascending=False) 
