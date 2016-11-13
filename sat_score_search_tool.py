685225a587fff1e37486c2a1781ed66a18dc0ac5
scott.underwood87 <scott.underwood87@gmail.com>
2016-11-13 12:54:39 -0500

    added notes and improved UI formatting

--- sat_score_search_tool.py
+++ sat_score_search_tool.py
@@ -24,13 +24,16 @@ raw_data_no_s['overall_average_score'] = (raw_data_no_s.sat_writing_avg_score + 
 
 
 # user input prompted to indicate which score they want to sort by, if they want highest or lowest scoring schools, and how many to show
-# added in print lines in attmpt to make the input prompts easier to see
+# breaking out lines printed in the console further so that they are easier to interact with
 print ""
-search_target = raw_input("What specific sat score are you looking to rank schools by? (math, writing, critical reading, overall) ")
+print "What specific sat score are you looking to rank schools by? (math, writing, critical reading, overall)"
+search_target = raw_input("Response: ")
 print ""
-search_type = raw_input("Are you looking for the best schools or the worst? ")
+print "Are you looking for the best schools or the worst schools?"
+search_type = raw_input("Response: ")
 print ""
-search_size = raw_input("How long a list do you need? ")
+print "How long a list do you need? "
+search_size = raw_input("Response: ")
 
 # NEED - create a function that tests search_size input to make sure its just a number
 
@@ -48,6 +51,7 @@ def search_function(s_type, s_target):
   # NEED - fix the invalid response so that it communicates what needs to be changed
 
   # an if statement that will translate s_target into the correct sorting column based on user input
+  # NEED - break search target validation out into its own function?
   while stop_trigger_1 == True:
     if s_target_lower == "math":
       s_target_input = "sat_math_avg_score"
@@ -66,7 +70,8 @@ def search_function(s_type, s_target):
       s_target_lower = raw_input("What specific sat score are you looking to rank schools by? (math, writing, critical reading, overall)")
   
   # an if statement that sorts scores high-low or low-high based on user input, also ensures that sort is also run on the right column of scores
-  # changed the sort value from "overall_average_score" to the value of the s_target_input variable  
+  # changed the sort value from "overall_average_score" to the value of the s_target_input variable 
+  # NEED - break search target validation out into its own function?
   while stop_trigger_2 == True:
     if s_type_lower == "best":
       sorted_raw_data_no_s = raw_data_no_s.sort_values(s_target_input, ascending=False) 
